e6583a235b57f0579571dab9d13966e92bc0e28a
leonsor-VAIO <34835973+leonsor@users.noreply.github.com>
2018-09-16 23:48:54 +0200

    all working except 2 max

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,5 +1,4 @@
 # When done, submit this entire file to the autograder.
-
 # Part 1
 def sum(arr)
   som = 0
@@ -16,12 +15,37 @@ def sum(arr)
   end
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  if arr.empty? # array empty
+    return 0
+  elsif arr.length == 1 # only 1 value in array
+    return arr[0]
+  else
+    arr1 = arr.max(2)
+    large1 = arr1[0]
+    large2 = arr1[1]
+  end
+  return large1 + large2
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  if arr.empty? # array empty
+    return false
+  elsif arr.length == 1 # only 1 value in array
+    return false
+  else
+    b = arr.chunk_while {|i, j| i + j == n }.to_a #if i+j n put in new array b
+    c = b.join
+    if c.empty? #check if b is empty (no match!)
+      return false
+    else
+      if c.length <= 2
+        return true
+      else
+        return false
+      end
+    end
+  end
 end
 
 # Part 2
